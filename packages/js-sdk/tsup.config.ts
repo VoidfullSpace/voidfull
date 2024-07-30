dc0f702b72be1125239b0549a91a01ee60d8298c
Yashu Mittal <mittalyashu77@gmail.com>
2024-07-30 12:56:35 +0530

    feat(js-sdk): add tsup config

--- packages/js-sdk/tsup.config.ts
+++ packages/js-sdk/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-	entry: ['./src/index.ts', './src/types.ts'],
+	entry: ['./src/index.ts'],
 	format: ['esm', 'cjs'],
 	dts: true,
 	minify: true,
